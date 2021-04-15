958c73791ffd1b67b795cbabf8ae05053493aea7
Pavel Gromov <pagrom27@gmail.com>
2021-04-15 22:45:18 +0300

    Create settings

--- app/src/main/java/com/example/alias/storage/GameSettings.kt
+++ app/src/main/java/com/example/alias/storage/GameSettings.kt
@@ -2,11 +2,9 @@ package com.example.alias.storage
 
 class GameSettings {
     companion object {
-        private const val ONE_SECOND = 1_000L
+//        private const val ONE_SECOND =
 
-        const val roundTime: Long = 3L * ONE_SECOND
+//        const val roundTime: Long = ONE_SECOND
         const val categoryResources: String = "categories"
-
-        var wrongPenalty = false
     }
 }
